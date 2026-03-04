c293bd0dc4d14af7a9a80cb610b752feee03ebf5
Richard Meiland <richard.meiland@gmail.com>
2026-03-04 11:33:16 +0100

    Final courseinfo version

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,35 +1,53 @@
-const Header = (props) => {
-  return (
-    <h1>{props.course}</h1>
-  )
-}
+// https://fullstackopen.com/en/part1/java_script
 
-const App = () => {
-  const course = "Half Stack application development";
-  const part1 = "Fundamtentals of React";
-  const exercise1 = 10;
-  const part2 = "Using props to pass";
-  const exercise2 = 7;
-  const part3 = "State of a component";
-  const exercise3 = 14;
+const Header = ({ course }) => {
+  return <h1>{course.name}</h1>;
+};
+
+const Part = ({ part }) => {
+  return (
+    <p>
+      {part.name}
+      {part.exercise}
+    </p>
+  );
+};
+
+const Content = ({ course }) => {
+  const { parts } = course;
 
   return (
     <div>
-      <Header course={course} />
-      <p>
-        {part1}{exercise1}
-      </p>
-      <p>
-        {part2}{exercise2}
-      </p>
-      <p>
-        {part3}{exercise3}
-      </p>
-      <p>
-        Number of exercises {exercise1 + exercise2 + exercise3}
-      </p>
+      <Part part={parts[0]} />
+      <Part part={parts[1]} />
+      <Part part={parts[2]} />
     </div>
   );
 };
 
-export default App
+const Total = ({ course }) => {
+  const { parts } = course;
+  const [exercise1, exercise2, exercise3] = parts.map((part) => part.exercise);
+  return <p>Number of exercises {exercise1 + exercise2 + exercise3}</p>;
+};
+
+const App = () => {
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      { name: "Fundamtentals of React", exercise: 10 },
+      { name: "Using props to pass", exercise: 7 },
+      { name: "State of a component", exercise: 14 },
+    ],
+  };
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content course={course} />
+      <Total course={course} />
+    </div>
+  );
+};
+
+export default App;
